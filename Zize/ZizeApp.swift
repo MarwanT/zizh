4379c3a0a23cd204c20ba41cd9c3c42da3a378e8
Marwan Tutunji <marwan.toutounji@gmail.com>
2025-01-29 14:58:18 +0200

    Initial Commit

---
+++ Zize/ZizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZizeApp.swift
+//  Zize
+//
+//  Created by Marwan Tutunji on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZizeTests/ZizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZizeTests.swift
+//  ZizeTests
+//
+//  Created by Marwan Tutunji on 29/01/2025.
+//
+
+import Testing
+@testable import Zize
+
+struct ZizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
